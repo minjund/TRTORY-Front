edbadc82e3c37f0db1b883a2296676d007dd4e20
minjund <mam060922@naver.com>
2023-12-06 23:49:37 +0900

    fix : 계획 변경
    시나리오 올리기에만 집중하여 개발하도록 설정

--- layouts/header.vue
+++ layouts/header.vue
@@ -6,75 +6,83 @@
       <nuxt-link to="/main" class="flex items-center">
         <img src="/images/logo.jpg" class="mr-3 h-10" alt="TRPG Logo" />
       </nuxt-link>
-      <div class="flex md:order-2">
-        <nuxt-link
-          to="/account"
-          class="mr-3 rounded-lg bg-blue-700 px-4 py-2 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 md:mr-0"
-          >로그인</nuxt-link
-        >
-        <button
-          data-collapse-toggle="navbar-sticky"
-          type="button"
-          class="inline-flex items-center rounded-lg p-2 text-sm text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600 md:hidden"
-          aria-controls="navbar-sticky"
-          aria-expanded="false">
-          <span class="sr-only">Open main menu</span>
-          <svg
-            class="h-6 w-6"
-            aria-hidden="true"
-            fill="currentColor"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg">
-            <path
-              fill-rule="evenodd"
-              d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-              clip-rule="evenodd"></path>
-          </svg>
-        </button>
-      </div>
       <div
         class="hidden w-full items-center justify-between md:order-1 md:flex md:w-auto"
         id="navbar-sticky">
-        <ul
-          class="mt-4 flex flex-col rounded-lg border border-gray-100 bg-gray-50 p-4 font-medium dark:border-gray-700 dark:bg-gray-800 md:mt-0 md:flex-row md:space-x-8 md:border-0 md:bg-white md:p-0 md:dark:bg-gray-900">
-          <li>
-            <nuxt-link
-              to="/scenario"
-              class="block rounded py-2 pl-3 pr-4 md:p-0 md:dark:text-blue-500"
-              :class="[
-                $route.path === '/scenario'
-                  ? 'bg-blue-700 text-white md:bg-transparent md:text-blue-700'
-                  : 'rounded text-gray-900 hover:bg-gray-100 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:p-0 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-blue-500',
-              ]"
-              >시나리오</nuxt-link
-            >
-          </li>
-          <li>
-            <a
-              href="#"
-              class="block rounded py-2 pl-3 pr-4 text-gray-900 hover:bg-gray-100 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:p-0 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-blue-500"
-              :class="{
-                'bg-blue-700 text-white': $route.path === '/reference',
-              }"
-              >창작 마당</a
-            >
-          </li>
-          <li>
-            <a
-              href="#"
-              class="block rounded py-2 pl-3 pr-4 text-gray-900 hover:bg-gray-100 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:p-0 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-blue-500"
-              >구인/구직</a
-            >
-          </li>
-          <li>
-            <a
-              href="#"
-              class="block rounded py-2 pl-3 pr-4 text-gray-900 hover:bg-gray-100 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:p-0 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-blue-500"
-              >행사</a
-            >
-          </li>
-        </ul>
+        TRPG의 시나리오를 자유롭게 작성하고 놀아보세요!
       </div>
+      <div
+        class="hidden w-full items-center md:order-1 md:flex md:w-auto"
+        id="navbar-sticky"></div>
+      <!--      <div class="flex md:order-2">-->
+      <!--        <nuxt-link-->
+      <!--          to="/account"-->
+      <!--          class="mr-3 rounded-lg bg-blue-700 px-4 py-2 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 md:mr-0"-->
+      <!--          >로그인</nuxt-link-->
+      <!--        >-->
+      <!--        <button-->
+      <!--          data-collapse-toggle="navbar-sticky"-->
+      <!--          type="button"-->
+      <!--          class="inline-flex items-center rounded-lg p-2 text-sm text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600 md:hidden"-->
+      <!--          aria-controls="navbar-sticky"-->
+      <!--          aria-expanded="false">-->
+      <!--          <span class="sr-only">Open main menu</span>-->
+      <!--          <svg-->
+      <!--            class="h-6 w-6"-->
+      <!--            aria-hidden="true"-->
+      <!--            fill="currentColor"-->
+      <!--            viewBox="0 0 20 20"-->
+      <!--            xmlns="http://www.w3.org/2000/svg">-->
+      <!--            <path-->
+      <!--              fill-rule="evenodd"-->
+      <!--              d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"-->
+      <!--              clip-rule="evenodd"></path>-->
+      <!--          </svg>-->
+      <!--        </button>-->
+      <!--      </div>-->
+      <!--      <div-->
+      <!--        class="hidden w-full items-center justify-between md:order-1 md:flex md:w-auto"-->
+      <!--        id="navbar-sticky">-->
+      <!--        <ul-->
+      <!--          class="mt-4 flex flex-col rounded-lg border border-gray-100 bg-gray-50 p-4 font-medium dark:border-gray-700 dark:bg-gray-800 md:mt-0 md:flex-row md:space-x-8 md:border-0 md:bg-white md:p-0 md:dark:bg-gray-900">-->
+      <!--          <li>-->
+      <!--            <nuxt-link-->
+      <!--              to="/scenario"-->
+      <!--              class="block rounded py-2 pl-3 pr-4 md:p-0 md:dark:text-blue-500"-->
+      <!--              :class="[-->
+      <!--                $route.path === '/scenario'-->
+      <!--                  ? 'bg-blue-700 text-white md:bg-transparent md:text-blue-700'-->
+      <!--                  : 'rounded text-gray-900 hover:bg-gray-100 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:p-0 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-blue-500',-->
+      <!--              ]"-->
+      <!--              >시나리오</nuxt-link-->
+      <!--            >-->
+      <!--          </li>-->
+      <!--          <li>-->
+      <!--            <a-->
+      <!--              href="#"-->
+      <!--              class="block rounded py-2 pl-3 pr-4 text-gray-900 hover:bg-gray-100 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:p-0 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-blue-500"-->
+      <!--              :class="{-->
+      <!--                'bg-blue-700 text-white': $route.path === '/reference',-->
+      <!--              }"-->
+      <!--              >창작 마당</a-->
+      <!--            >-->
+      <!--          </li>-->
+      <!--          <li>-->
+      <!--            <a-->
+      <!--              href="#"-->
+      <!--              class="block rounded py-2 pl-3 pr-4 text-gray-900 hover:bg-gray-100 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:p-0 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-blue-500"-->
+      <!--              >구인/구직</a-->
+      <!--            >-->
+      <!--          </li>-->
+      <!--          <li>-->
+      <!--            <a-->
+      <!--              href="#"-->
+      <!--              class="block rounded py-2 pl-3 pr-4 text-gray-900 hover:bg-gray-100 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:p-0 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-blue-500"-->
+      <!--              >행사</a-->
+      <!--            >-->
+      <!--          </li>-->
+      <!--        </ul>-->
+      <!--      </div>-->
     </div>
   </nav>
 </template>
